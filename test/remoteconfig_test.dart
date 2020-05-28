f87235e38395f1eab162815fbcc5d7b1c466e9d9
Drawner <gtfperry@gmail.com>
2020-05-27 23:07:07 -0500

    1.0.0 Format file.

--- test/remoteconfig_test.dart
+++ test/remoteconfig_test.dart
@@ -3,7 +3,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:remote_config/remote_config.dart';
 
 void main() {
-  test('empty test', () {
-
-  });
+  test('empty test', () {});
 }
